06ef39eb12df1d0a6f661679da877922d2f0f81e
Habeeb Hussain <u1262813@unimail.hud.ac.uk>
2016-04-29 16:23:40 +0100

    Update
    Updated the nav bar links as well as adding some user restrictions as to when they are not logged in.

--- application/views/home.php
+++ application/views/home.php
@@ -15,10 +15,10 @@
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
                     <li>
-                    <a href="<?php echo site_url("welcome/addjobForm");?>">Add song</a>
+                    <a href="<?php echo site_url("welcome/registration");?>">Add song</a>
                     </li>
                     <li>
-                        <a href="<?php echo site_url("welcome/allSongs");?>">All songs</a>
+                        <a href="<?php echo site_url("welcome/registration");?>">All songs</a>
                     </li>
                     <li>
                         <a href="<?php echo site_url("welcome/registration");?>">Login/Register</a>
@@ -39,22 +39,6 @@
                 <h1 class="page-header">SongUniverse
                     <small>Welcome to SongUniverse, a place for you to find out how your favourite songs are performing on the charts around the world. These charts include the Billboard Hot 100, The official UK top 40 and the Canadian Hot 100. Register for an account today to be able to submit your song.</small>
                 </h1>
-            </div>
-        </div>
-        <!-- /.row -->
-
-         <!-- Project One -->
-        <div class="row">
-            <div class="col-md-7">
-                <a href="#">
-                    <img class="img-responsive" src="https://placehold.it/700x300" alt="">
-                </a>
-            </div>
-            <div class="col-md-5">
-                <h3>Project One</h3>
-                <h4>Subheading</h4>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium veniam exercitationem expedita laborum at voluptate. Labore, voluptates totam at aut nemo deserunt rem magni pariatur quos perspiciatis atque eveniet unde.</p>
-                <a class="btn btn-primary" href="#">View Project <span class="glyphicon glyphicon-chevron-right"></span></a>
             </div>
         </div>
         <!-- /.row -->
